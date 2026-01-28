01d928a774a316cd16d7691190eaf69a25ef5148
NicolasFrance01 <francenicolas.dw@gmail.com>
2026-01-28 12:29:23 -0300

    Configure Vite for MPA to include sonica.html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/no-apto-para-bobos/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve('index.html'),
+                sonica: resolve('sonica.html'),
+            },
+        },
+    },
 })
